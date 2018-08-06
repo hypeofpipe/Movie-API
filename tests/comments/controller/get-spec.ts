5fc81320d4f5ccade41632f91957979a0fa2609c
Volodymyr <hypeofpipe@gmail.com>
2018-08-06 23:59:12 +0200

    Dep
    Created the basic project structure, added tests to Entities

---
+++ tests/comments/controller/get-spec.ts
@@ -0,0 +1,8 @@
+import {expect} from 'chai'
+import 'mocha'
+
+describe('Testing the GET /movies endpoint', () => {
+    it('Test', () => {
+        expect('test').to.equal('test')
+        })
+})
---
+++ tests/comments/entity/entity-spec.ts
@@ -0,0 +1,49 @@
+import { expect } from "chai"
+import "mocha"
+import { Connection } from "typeorm"
+import { createConnection } from "typeorm"
+import { Comments } from "../../../src/entity/Comments";
+import { Movie } from "../../../src/entity/Movie";
+
+describe("Testing an movie entity", () => {
+  let connection: Connection
+
+  beforeEach(async () => {
+    connection = await createConnection({
+      type: "sqljs",
+      entities: [Comments],
+      logging: false,
+      dropSchema: true,
+      synchronize: true
+    })
+  })
+
+  afterEach(async () => {
+    await connection.close()
+  })
+
+  it("testing if entity has been created and stored well", async () => {
+    
+    const comments: Comments = new Comments()
+    const movie: Movie = new Movie()
+   
+    movie.id = 1
+    movie.title = 'Bee Movie'
+    movie.year = 2007
+    movie.rated = 'PG'
+    movie.released = "02 Nov 2007"
+   
+    await connection.getRepository(Movie).save(movie)
+
+    comments.id = 1
+    comments.author = 'Testosteron'
+    comments.movie = movie
+    comments.content = 'I was really impressed by this movie!'
+
+    await connection.getRepository(Comments).save(comments)
+    
+    const result = await connection.getRepository(Comments).findOneOrFail(1)
+
+    expect(result).to.equal(comments)
+  })
+})
